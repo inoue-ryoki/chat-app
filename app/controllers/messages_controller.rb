170305fd89032fd74b74769675a0fdcb23d930eb
inoue-ryoki <mth.1021@icloud.com>
2022-05-24 18:30:12 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,7 +27,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-   params.require(:message).permit(:content).merge(user_id: current_user.id)
+   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
   # メッセージの内容とuser idの中にcurrent_user.idを入れる
   # mergeは追加する
