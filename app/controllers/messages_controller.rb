06a1c34e4a3e26bb1252a63056c843ca77b237d6
inoue-ryoki <mth.1021@icloud.com>
2022-05-24 16:46:15 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,32 @@
 class MessagesController < ApplicationController
   def index
+
+    @message= Message.new
+     @room = Room.find(params[:room_id])
+
   end
+
+  def create
+    # ボタンを押した瞬間発動する
+
+    @room = Room.find(params[:room_id])
+    # どのルームか判別する
+    @message = @room.messages.new(message_params)
+    # どのルームのメッセージか判別してメッセージの内容を入れる
+       if @message.save
+    # メッセージが保存できたら保存後の情報に更新され、メッセージ画面にもどる
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+
+  end
+
+  private
+
+  def message_params
+   params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+  # メッセージの内容とuser idの中にcurrent_user.idを入れる
+  # mergeは追加する
 end
